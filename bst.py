4211089d4309140366b00c02fed115600ed3da14
Sid <izzyy.wq@gmail.com>
2023-05-09 10:45:52 -0400

    fixed functionality

--- bst.py
+++ bst.py
@@ -1,29 +1,35 @@
-class Node:
-  def __init__(self, value, left=None, right=None):
-    self.value = value
-    self.left = left
-    self.right = right
+class BST:
+	def __init__(self, value, left=None, right=None, parent=None):
+		self.value = value
+		self.left = left
+		self.right = right
+		self.parent = parent
 
-def search(root: Node, target):
-  """
-  Binary-search algorithm
-  """
-  if root is None:
-    return None
-  elif root.value < target:
-    return search(root.right, target)
-  else:
-    return search(root.left, target)
-    
-  return root
-
-root = Node(10)
-root.left = Node(5)
-root.right = Node(15)
-root.left.left = Node(2)
-root.left.right = Node(7)
-root.right.left = Node(12)
-root.right.right = Node(20)
+def search(root: BST, target):
+    """
+    Binary search algorithm.
+    """
+	# target is not in the BST
+    if root is None:
+        return None
+	# target found
+    elif root.value == target:
+        return root
+	# search the right side of the tree
+    elif root.value < target:
+        return search(root.right, target)
+	# search the left side of the tree
+    else:
+        return search(root.left, target)
 
 
-search(root, 7)
+# Sample binary tree for testing
+root = BST(10)
+root.left = BST(5, parent=root)
+root.right = BST(15, parent=root)
+root.left.left = BST(2, parent=root.left)
+root.left.right = BST(7, parent=root.left)
+root.right.left = BST(12, parent=root.right)
+root.right.right = BST(20, parent=root.right)
+
+target_node = search(root, 7)
