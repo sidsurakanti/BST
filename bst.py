6add8ab8e02e36d8eaf4237a8adf8922770fe0d4
Sid <sidsurakanti1@gmail.com>
2023-05-12 14:02:24 -0400

    added a bunch of methods

--- bst.py
+++ bst.py
@@ -5,7 +5,59 @@ class BST:
 		self.right = right
 		self.parent = parent
 
-	def search(self, target):
+	def height(self, tree) -> int:
+		"""
+  		Finds height of :param tree:
+		
+ 		:param tree: <BST>
+   		:returns: int
+  		"""
+		if tree:
+			# find height of left subtree recursively
+			left_height = self.height(tree.left)
+			# find height of right subtree recursively
+			right_height = self.height(tree.right)
+			return max(left_height, right_height) + 1
+		return 0
+
+	@property
+	def inorder(self):
+		def traverse(node, res):
+			if node:
+				# traverse left subtree
+				traverse(node.left, res)
+				# append middle node value
+				res.append(node.value)
+				# traverse right subtree
+				traverse(node.right, res)
+
+		res = []
+		traverse(self, res)
+		return res
+
+	def min(self):
+		"""
+  		Returns minimum value in the BST
+
+  		:returns: <BST>
+  		"""
+		cur = self
+		# stop iterating if at the end of the BST
+		while cur.left is not None:
+			cur = cur.left
+		# return last element searched
+		return cur
+
+	@property
+	def is_empty(self):
+		"""
+  		Checks if tree is empty
+
+  		:returns: bool
+  		"""
+		return False if self.right and self.left else True
+
+	def _search(self, target):
 		"""
 	    Search for a node in the binary search tree with the given value.
 
@@ -23,27 +75,29 @@ class BST:
 			return self
 		# search the right side of the tree
 		elif self.value < target:
-			return self.right.search(target) if self.right else None
+			return self.right._search(target) if self.right else None
 		# search the left side of the tree
 		else:
-			return self.left.search(target) if self.left else None
-
-	def height(self, tree) -> int:
+			return self.left._search(target) if self.left else None
+	
+	def __getitem__(self, value):
 		"""
-  		Finds height of :param tree:
-		
- 		:param tree: 
+  		Enables object indexing. BST[i]
+
+  		:params value: value to search for in BST
+		:returns: 
+  			<BST>: Node containing target value
   		"""
-		if tree:
-			# find height of left subtree recursively
-			left_height = self.height(tree.left)
-			# find height of right subtree recursively
-			right_height = self.height(tree.right)
-			return max(left_height, right_height) + 1
-		return 0
+		return repr(self._search(value))
 
+	def __str__(self):
+		return ", ".join([str(n) for n in self.inorder])
+	
+	def __repr__(self):
+		return f"BST({self.inorder})"
 
-# Sample binary tree for testing
+	
+# example binary tree for testing
 root = BST(10)
 root.left = BST(5, parent=root)
 root.right = BST(15, parent=root)
@@ -52,7 +106,14 @@ root.left.right = BST(7, parent=root.left)
 root.right.left = BST(12, parent=root.right)
 root.right.right = BST(20, parent=root.right)
 
-target = root.search(20)
 height = root.height(root)
-print(target.value if target else None)
-print(height)
+inorder = root.inorder
+empty = root.is_empty
+min_val = root.min().value
+
+print(f"Height: {height}")
+print(f"Inorder Traversal: {inorder}")
+print(f"Is empty: {empty}")
+print(f"Min value: {min_val}")
+print(root)
+print(root[5])
